921b437145967fca9c95b515d45451a295f4e8c4
butrintrashiti <buti.96@gmail.com>
2020-02-15 14:52:33 +0100

    Folder Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ProductsModule } from "../modules/products/products.module";
+import { CoreModule } from "../modules/core/core.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [CoreModule, BrowserModule, AppRoutingModule, ProductsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/modules/products/components/crypto-currencies/crypto-currencies.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-crypto-currencies",
+  templateUrl: "./crypto-currencies.component.html",
+  styleUrls: ["./crypto-currencies.component.scss"]
+})
+export class CryptoCurrenciesComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
